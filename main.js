09bcb05cb483856221b48aced3efdb91f3fd3a97
Andres Felipe Cubillos <OspinaFelipeDev@gmail.com>
2023-12-13 12:12:29 -0500

    Se hizo la clast list de productos con jacascript

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shoping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -40,3 +41,77 @@ function toggleCarritoAside() {
     aside.classList.toggle('inactive');
 
 }
+
+const productList = [];
+productList.push({
+    name: 'Figure',
+    price: 120,
+    image: 'https://images.pexels.com/photos/16075339/pexels-photo-16075339/free-photo-of-munecas-juguetes-fondo-gris-fondo-de-pantalla-para-el-movil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Bike',
+    price: 150,
+    image: 'https://images.pexels.com/photos/16075339/pexels-photo-16075339/free-photo-of-munecas-juguetes-fondo-gris-fondo-de-pantalla-para-el-movil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Tv',
+    price: 200,
+    image: 'https://images.pexels.com/photos/16075339/pexels-photo-16075339/free-photo-of-munecas-juguetes-fondo-gris-fondo-de-pantalla-para-el-movil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Figure',
+    price: 120,
+    image: 'https://images.pexels.com/photos/16075339/pexels-photo-16075339/free-photo-of-munecas-juguetes-fondo-gris-fondo-de-pantalla-para-el-movil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Bike',
+    price: 150,
+    image: 'https://images.pexels.com/photos/16075339/pexels-photo-16075339/free-photo-of-munecas-juguetes-fondo-gris-fondo-de-pantalla-para-el-movil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Tv',
+    price: 200,
+    image: 'https://images.pexels.com/photos/16075339/pexels-photo-16075339/free-photo-of-munecas-juguetes-fondo-gris-fondo-de-pantalla-para-el-movil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+     
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+     
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+     
+        const productInfoDiv = document.createElement('div');
+     
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+     
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+     
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+     
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', 'icons/bt_add_to_cart.svg');
+     
+        productInfoFigure.appendChild(productImgCart);
+     
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+     
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+     
+         cardsContainer.appendChild(productCard);
+     }
+}
+
+renderProducts(productList);
